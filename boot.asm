9548262f910ed807322b8a5c3a0727d269254b58
Myeongjun Go <myeongjun.ko@gmail.com>
2018-01-29 01:11:12 +0900

    protected mode

--- boot.asm
+++ boot.asm
@@ -2,20 +2,21 @@
   jmp 07C0h:start
 
 start:
-	mov ax, cs
-	mov ds, ax
+  mov ax, cs
+  mov ds, ax
+  mov es, ax
 
-	mov ax, 0xB800
-	mov es, ax
-	mov di, 0
-	mov ax, word [msgBack]
-	mov cx, 0x7FF
+  mov ax, 0xB800
+  mov es, ax
+  mov di, 0
+  mov ax, word [msgBack]
+  mov cx, 0x7FF
 
 paint:
-	mov word [es:di], ax
-	add di, 2
-	dec cx
-	jnz paint
+  mov word [es:di], ax
+  add di, 2
+  dec cx
+  jnz paint
 
 read:
   mov ax, 0x1000
